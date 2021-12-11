5f4f4a7010c8e98ceb05a3b2505bf2397b62db65
Potato <w17666104677@outlook.com>
2021-12-11 20:51:58 +0800

    bulletinboard

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//Buefy
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+//ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(Buefy)
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
