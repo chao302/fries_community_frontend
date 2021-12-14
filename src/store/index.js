8adcd98b64b7bd9fecf16f016883abc5e00b6cbf
Potato <w17666104677@outlook.com>
2021-12-15 00:30:49 +0800

    侧边栏-入驻&登录

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import user from './modules/user'
+import Vue from "vue";
+import Vuex from "vuex";
+import getters from "./getters";
+import user from "./modules/user";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
-      user
-  }
-})
+    user,
+  },
+  getters,
+});
 
-export default store
+export default store;
--- src/views/card/LoginWelcome.vue
+++ src/views/card/LoginWelcome.vue
@@ -3,17 +3,29 @@
     <div slot="header">
       <span>💐 发帖</span>
     </div>
-    <div>body</div>
+    </div>
+    <div v-if="token != null && token !== ''" class="has-text-centered">
+      <b-button type="is-danger" tag="router-link" :to="{path:'/post/create'}" outlined>✍ 发表想法</b-button>
+    </div>
+
+    <div v-else class="has-text-centered">
+      <b-button type="is-primary" tag="router-link" :to="{path:'/register'}" outlined>即刻入驻</b-button>
+      <b-button type="is-danger" tag="router-link" :to="{path:'/login'}" outlined class="ml-2">用户登录</b-button>
+    </div>
   </el-card>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: "LoginWelcome",
-  data() {
-    return {};
-  },
-};
+  computed:{
+    ...mapGetters([
+      'token'
+    ])
+  }
+}
 </script>
 
 <style scoped></style>
