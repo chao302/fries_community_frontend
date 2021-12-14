374d36e142e458396d1d794a5b22e46341d8db5d
Potato <w17666104677@outlook.com>
2021-12-14 12:21:49 +0800

    主要界面

--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
 //Buefy
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 
 //ElementUI
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
-Vue.use(Buefy)
+import "@/assets/app.css";
+Vue.use(Buefy);
 Vue.use(ElementUI);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
