3ad7321d02601294290c0ccdac2d8e1d5e4511d4
Potato <w17666104677@outlook.com>
2021-12-16 18:11:25 +0800

    整合

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -16,7 +16,7 @@
       </template>
       <template slot="start">
         <b-navbar-item tag="router-link" :to="{ path: '/' }">
-          🌐 主页
+          主页
         </b-navbar-item>
       </template>
 
@@ -63,23 +63,9 @@
             </b-button>
           </div>
         </b-navbar-item>
-
         <b-navbar-dropdown v-else :label="user.alias">
-          <b-navbar-item
-            tag="router-link"
-            :to="{ path: `/member/${user.username}/home` }"
-          >
-            🧘 个人中心
-          </b-navbar-item>
           <hr class="dropdown-divider" />
-          <b-navbar-item
-            tag="router-link"
-            :to="{ path: `/member/${user.username}/setting` }"
-          >
-            ⚙ 设置中心
-          </b-navbar-item>
-          <hr class="dropdown-divider" />
-          <b-navbar-item tag="a" @click="logout"> 👋 退出登录 </b-navbar-item>
+          <b-navbar-item tag="a" @click="logout"> 退出登录 </b-navbar-item>
         </b-navbar-dropdown>
       </template>
     </b-navbar>
